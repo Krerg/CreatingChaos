6eba7659e1be1a6c8e37b5ee2677533f8074ab0d
Denis_Ruchkin <den21021996@gmail.com>
2020-05-05 18:25:20 +0300

    LD3

---
+++ Assets/Destroy_platforms.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_platforms : MonoBehaviour
+{
+    public GameObject platform1;
+    public GameObject platform2;
+    public GameObject platform3;
+    public GameObject platform4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+            if (col.gameObject.tag == "box")
+            {
+                Destroy(platform1);
+                Destroy(platform2);
+                Destroy(platform3);
+                Destroy(platform4);
+            }
+
+
+    }
+}
---
+++ Assets/NextDialog.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextDialog : MonoBehaviour
+{
+    public bool showed;
+    public int newIndex;
+    public GameObject dialogManager;
+    public bool I_need_tom;
+    public bool tom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tom = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (tom == true)
+                tom = false;
+            else
+                tom = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (showed == false)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                if (I_need_tom == tom)
+                {
+                    dialogManager.GetComponent<Dialog>().NextSentence(newIndex);
+                    showed = true;
+                }
+            }
+        }
+
+    }
+}
+
